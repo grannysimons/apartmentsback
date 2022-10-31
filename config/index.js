d45e5f4b0c4f6e6fd944a5717fb1257d9457fed3
Mariona Roca <marionaroca@MacBook-MacBook-Pro-de-Mariona.local>
2022-10-31 11:08:38 +0100

    cors canviats

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: ["http://holaquetal.com"]
+      origin: ["https://apartmentsfront.herokuapp.com/"]
     })
   );
 
